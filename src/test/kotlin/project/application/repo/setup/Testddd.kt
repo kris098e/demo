bca71f2674f4efa3c8177e0efaad4f85b11a769a
kris098e <abrahamsenkd@gmail.com>
2023-10-25 14:38:39 +0200

    example working test database

--- src/test/kotlin/project/application/repo/setup/Testddd.kt
+++ src/test/kotlin/project/application/repo/setup/Testddd.kt
@@ -1,16 +1,28 @@
 package project.application.repo.setup
 
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.jooq.generated.tables.records.UsersRecord
 import org.junit.jupiter.api.Test
 import project.repository.TestRepo
 
-class Testddd : TestConfig() {
+@MicronautTest
+class Testddd {
 
-    private val repo = TestRepo(dslContext)
+
+    @Inject
+    lateinit var repo: TestRepo
 
     @Test
     fun test() {
+        repo.dslContext.insertInto(org.jooq.generated.tables.Users.USERS).set(UsersRecord().apply {
+            this.username = "hans"
+            this.password = "password"
+            this.enabled = true
+        }).execute()
+
         val testRepo = repo.getUsers("hans")
 
-        println(testRepo)
+        println(testRepo.size)
     }
 }
